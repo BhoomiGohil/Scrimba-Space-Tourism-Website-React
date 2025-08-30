89487a88bb2df8e230f4395b3e7d4996bb6027c0
Bhoomi Gohil <26293804+BhoomiGohil@users.noreply.github.com>
2025-08-29 22:17:30 -0400

    Added Navigation Component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+import data from "../data.json";
+
+const Navigation = () => {
+  var { navItems } = data;
+
+  return (
+    <div className="navigation flex align-center justify-between">
+      <img src={"assets/shared/logo.svg"} className="logo" />
+      <div className="navigation-divider divider-white"></div>
+      <input type="checkbox" id="navigation-mobile-menu-button" />
+      <label
+        htmlFor="navigation-mobile-menu-button"
+        className="navigation-mobile-menu"
+      ></label>
+      <div className="navigation-menu-container flex align-center">
+        {navItems.map((element, index) => (
+          <NavLink
+            key={index}
+            to={element.url}
+            className={({ isActive }) =>
+              `navigation-menu ${
+                isActive ? "active" : ""
+              } flex align-center fs-8 ff-barlow-cond letter-spacing-2 uppercase`
+            }
+          >
+            <span className="bold letter-spacing-207">
+              {String(index).padStart(2, "0")}
+            </span>
+            {element.name}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
