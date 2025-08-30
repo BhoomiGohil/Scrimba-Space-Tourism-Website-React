28dce4af716f652ed4f43c6b58fd38192aacb8f7
Bhoomi Gohil <26293804+BhoomiGohil@users.noreply.github.com>
2025-08-29 22:18:03 -0400

    Created Home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+import Navigation from "../components/Navigation";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  const desktop = "/assets/home/background-home-desktop.jpg";
+  const tablet = "/assets/home/background-home-tablet.jpg";
+  const mobile = "/assets/home/background-home-mobile.jpg";
+
+  const [bg, setBg] = useState(desktop);
+
+  useEffect(() => {
+    const updateBackground = () => {
+      var width = window.innerWidth;
+      setBg(width <= 735 ? mobile : width <= 832 ? tablet : desktop);
+    };
+
+    updateBackground();
+    window.addEventListener("resize", updateBackground);
+
+    return () => window.removeEventListener("resize", updateBackground);
+  });
+
+  return (
+    <div
+      className="main"
+      style={{
+        background: `url(${bg}) no-repeat top/cover`,
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <Navigation />
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
